d32c012876d27551e8e2758bfef3c59c1d2e9f5b
Esther Lee <el686@drexel.edu>
2024-12-14 19:30:51 -0500

    fixed index.html

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // script.js
+<script>
 function toggleMenu() {
     const navLinks = document.getElementById('nav-links');
+    const exitButton = document.querySelector('.exit-button');
+    
     navLinks.classList.toggle('active');
+    exitButton.classList.toggle('active');
 }
+</script>
